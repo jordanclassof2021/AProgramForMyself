328e898c366cbbc25e1962df5af0aaea90d9bc80
jordanclassof2021 <31080225+jordanclassof2021@users.noreply.github.com>
2017-10-01 12:50:00 -0400

    Initial commit

---
+++ src/Programformyself.java
@@ -0,0 +1,6 @@
+public class Programformyself {
+    public static void main( String [] args) {
+        // my name and how old I am
+        System.out.println("Jordan Lewis; 18 years old");
+    }
+}
